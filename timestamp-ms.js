1b4baaef3ccd7a2f6cee871094823195b6146ee9
nicholascm90 <nicholascm90@gmail.com>
2016-02-12 18:19:13 +0000

    hello world

--- timestamp-ms.js
+++ timestamp-ms.js
@@ -20,9 +20,7 @@ app.get('/', function(req, res) {
 })
 */
 app.get('/', function (req,res) {
-    res.end({
-            "unixtime": "something unixy"
-        }); 
+    res.end("hello world"); 
 }); 
 
 app.listen(app.get('port'), function() {
