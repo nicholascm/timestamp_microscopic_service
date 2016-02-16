d663d0dd88e7ee6e6cbab9f435aee98221841585
nicholascm90 <nicholascm90@gmail.com>
2016-02-16 11:09:35 +0000

    json response

--- timestamp-ms.js
+++ timestamp-ms.js
@@ -7,26 +7,22 @@ var date = new Date();
 
 app.set('port', (process.env.PORT || 5000));
 
-/*
 
-var date = Date()
-
-app.get('/', function(req, res) {
-    var query = req.query; 
-    res.send({
-            "unixtime": date.getTime()
-        })
-    
-})
-*/
 app.get('/', function (req,res) {
     res.send("hello world"); 
 }); 
 
 app.get('/test', function (req, res) {
-    res.send("test page"); 
+    res.send(JSON.stringify(getTimes(timeProvided))); 
 }); 
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var timeProvided = 1450137600; 
+
+function getTimes(num) {
+    var date = new Date(num); 
+    return {"unix": date.getTime()}
+}; 
