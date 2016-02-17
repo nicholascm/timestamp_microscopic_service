dbe0c6cab470f49949b3ea615fbc50553156351a
nicholascm90 <nicholascm90@gmail.com>
2016-02-17 10:26:48 +0000

    looking at parameters for URL, modified URL structure

--- timestamp-ms.js
+++ timestamp-ms.js
@@ -8,11 +8,13 @@ var date = new Date();
 app.set('port', (process.env.PORT || 5000));
 
 
-app.get('/', function (req,res) {
+app.get('/fun', function (req,res) {
     res.send("hello world"); 
 }); 
 
-app.get('/test', function (req, res) {
+app.get('/', function (req, res) {
+    var timeProvided = req.params.name; 
+    
     res.send(JSON.stringify(getTimes(timeProvided))); 
 }); 
 
@@ -20,15 +22,21 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-var timeProvided = 1450137600; 
+//var timeProvided = 1450137600; 
+
+
+
 
 function getTimes(num) {
     var date = new Date(num); 
     return {
         "unix": date.getTime(), 
-        "natural": monthString(date.getMonth())+" "+date.getDay()+","+date.getYear()
+        "natural": monthString(date.getMonth())+" "+date.getDay()+","+date.getFullYear()
     }
 }; 
+
+
+
 
 function monthString(num) {
     var months = ["January", "February", "March", "April", "May", "June"]
